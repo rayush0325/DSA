3a5f3fbbf61683bde3993935c3fa43499d8b8b65
rayush0325 <145927717+rayush0325@users.noreply.github.com>
2024-02-11 17:34:21 +0530

    Single-InsertAtBeg

--- LinkedList.java
+++ LinkedList.java
@@ -1,25 +1,33 @@
 public class LinkedList {
     //default constructor initializes class variable-head of type-Node with default value-null
-    static Node head;
+    Node head;
     //now the value of head is null
+    void insertNode(int value){
+        //non-static variable head accessible in non-static method insertNode
+        Node node=new Node();
+        node.data=value;
+        node.next=head;
+        head=node;
+    }
     public static void main(String[] args) {
-
-        Node node1=new Node();//node1 containing the reference to object created in heap
-        node1.data=1;//initialising members of object through dot syntax
-        node1.next=head;//value of variable-next of object is null
-        head=node1;//now head is containing the reference to this object
-
-        Node node2=new Node();//node2 containing the reference to 2nd object created in heap
-        node2.data=2;
-        node2.next=head;//now next of node2 is containing the reference to first object
-        head=node2;//now head is containing the reference to 2nd object
-
-        System.out.println(head.data);
-        System.out.println(head.next.data);
-        System.out.println(head.next.next);
+        //non-static method insetNode is accessible in static method by creating an object of class-LinkedList
+        LinkedList list=new LinkedList();
+        list.insertNode(1);
+        list.insertNode(2);
+        list.insertNode(3);
+        list.insertNode(4);
+        list.insertNode(5);
+        Node temp=list.head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
     }
 }
 class Node{
     int data;
     Node next;
+}
+class NodeCreation{
+
 }
